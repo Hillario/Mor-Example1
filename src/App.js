487cdae83c4483800f3e26aabe87d1edd001d703
hillario <hillches@gmail.com>
2022-03-23 20:24:13 +0300

    initialCommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
+/**
+ * Presentation Layer or Front-End of our application, import a csv file and display on
+ * a dynamic table for crud operations.
+ */
+
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  
+  //1 -->Hold data from csv file
+  //This is a hook, they let you use state and other React features without writing a class.
+  const [file, setFile] = useState();
+  const [array, setArray] = useState([]);
+  
+  //2 --> Read file using file reader
+  const fileReader = new FileReader();
+
+  //3 --> Form elements change
+  const handleOnChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  //4 --> convert file that was read as a plain text type into an Array of Object
+  const csvFileToArray = string => {
+    const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
+    const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
+
+    const array = csvRows.map(i => {
+      const values = i.split(",");
+      const obj = csvHeader.reduce((object, header, index) => {
+        object[header] = values[index];
+        return object;
+      }, {});
+      return obj;
+    });
+
+    setArray(array);
+  };
+
+  
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (file) {
+      fileReader.onload = function (event) {
+        const text = event.target.result;
+        csvFileToArray(text);
+      };
+
+      fileReader.readAsText(file);
+    }
+  };
+
+  const headerKeys = Object.keys(Object.assign({}, ...array));
+
+  //5 --> Display data to a table
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ textAlign: "center" }}>
+      <h1>REACTJS CSV IMPORT EXAMPLE </h1>
+      <form>
+        <input className='btn btn-contact'
+          type={"file"}
+          id={"csvFileInput"}
+          accept={".csv"}
+          onChange={handleOnChange}
+        />
+
+        <button className='btn btn-edit'
+          onClick={(e) => {
+            handleOnSubmit(e);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          IMPORT CSV
+        </button>
+      </form>
+
+      <br />
+
+      <table className='styled-table'>
+        <thead>
+          <tr key={"header"}>
+            {headerKeys.map((key) => (
+              <th>{key}</th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {array.map((item) => (
+            <tr key={item.id}>
+              {Object.values(item).map((val) => (
+                <td>{val}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
